b44cdff63381741b412ae502876215175235e3b6
Stefan Guggisberg <sguggisb@adobe.com>
2022-06-16 14:51:04 +0200

    add x-byo-cdn-type header

--- src/index.mjs
+++ src/index.mjs
@@ -17,6 +17,7 @@ const handleRequest = async (request, env, ctx) => {
   url.hostname = env.ORIGIN_HOSTNAME;
   const req = new Request(url, request);
   req.headers.set('x-forwarded-host', req.headers.get('host'));
+  req.headers.set('x-byo-cdn-type', 'cloudflare');
   // TODO: set the following header if push invalidation is configured
   // (see https://www.hlx.live/docs/setup-byo-cdn-push-invalidation#cloudflare)
   // req.headers.set('x-push-invalidation', 'enabled');
